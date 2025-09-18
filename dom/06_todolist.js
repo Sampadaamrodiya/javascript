48e9185765bf4617d841a662d8f23f7bb388ff35
Sampada Amrodiya <sampadaamrodiya@gmail.com>
2025-09-18 10:44:05 +0530

    todolist

--- dom/06_todolist.js
+++ dom/06_todolist.js
@@ -2,13 +2,36 @@ let inp =document.querySelector('input')
 let btn=document.querySelector("button")
 let ul=document.querySelector("ul")
 
+    // btn.addEventListener("click",()=>{
+    //      let data =inp.value
+    //     //  console.log(data)
+    //     let li=document.createElement("li")
+    //     li.innerText=data
+    //     ul.append(li)
+
+    //     inp.value="" //remove input value when work is done
+
+    //     li.addEventListener("click",()=>{
+    //        li.remove()
+    //     })
+
+    //     let btn2=document.querySelector("#delete")
+    //     btn2.addEventListener("click",()=>{
+    //         ul.innerText=""
+    //     })
+    // })
+
     btn.addEventListener("click",()=>{
-         let data =inp.value
-        //  console.log(data)
+        let data=inp.value;
         let li=document.createElement("li")
         li.innerText=data
         ul.append(li)
+        inp.value=""
 
-        inp.value="" //remove input value when work is done
+        let items=document.querySelector(li)
+        let list=[]
+        list.push(li.innerText)
+
 
     })
+

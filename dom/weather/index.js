9abbc60d0e615bc64a867ed1b12a4f58b885228b
Sampada Amrodiya <sampadaamrodiya@gmail.com>
2025-09-18 11:15:43 +0530

    weather api

---
+++ dom/weather/index.js
@@ -0,0 +1,16 @@
+
+
+
+let key="9f6290d6cda9a36a63755fadee71f83d"
+let city="jabalpur"
+
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`).
+then((data)=>{
+    return data.json()
+
+}).then((res)=>{
+    console.log(res);
+    
+
+})
